306addb0bdc8e971e6598493d4904333d66ada6b
jacksonohallo <jacksonohallo@gmail.com>
2022-12-12 10:17:54 -0800

    style: style layout section

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import React from "react";
 
 const Layout = ({ title, children }) => {
@@ -10,11 +11,26 @@ const Layout = ({ title, children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className=" flex min-h-screen flex-col justify-between">
-        <header>header</header>
-        <main>{children}</main>
+      <div className=" flex min-h-screen flex-col justify-between ">
+        <header>
+        <nav className="flex h-12 justify-between items-center px-4 shadow-md ">
+            <Link href="/" className="text-lg font-bold">
+           amazona
+            </Link>
+        
+        <div>
+         <Link href="/cart" className="p-2">Cart</Link>
+         <Link href="/login" className="p-2">Login</Link>
 
-        <footer>footer</footer>
+        </div>
+
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+
+        <footer className="flex h-10 items-center shadow-inner justify-center">
+            <p> Copyright 2022  Amazona</p>
+        </footer>
       </div>
     </>
   );
