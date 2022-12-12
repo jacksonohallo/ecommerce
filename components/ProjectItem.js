a367daa7b7426cded31deddca4c2459339c93f57
jacksonohallo <jacksonohallo@gmail.com>
2022-12-12 11:15:24 -0800

    add display products

---
+++ components/ProjectItem.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+
+const ProjectItem = ({ product }) => {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
+        </Link>
+        <p className="mt-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
