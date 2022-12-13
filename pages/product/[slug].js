6fd7ba2316dc9f41e479b6d9072fcb575ad09091
jacksonohallo <jacksonohallo@gmail.com>
2022-12-13 04:06:58 -0800

    feat: create price card

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import Layout from '../../components/Layout'
 import data from '../../utilis/data';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const ProductScreen = () => {
 const {query} = useRouter();
@@ -16,6 +17,43 @@ if (!product){
 <Layout title={product.name}>
     <div className="py-2">
   <Link href="/">back to products</Link>
+   <div className="grid md:grid-cols-4 md:gap-3">
+    <div className="md:col-span-2">
+      <Image
+      src={product.image}
+      alt={product.name}
+      width={640}
+      height={640}
+      layout="responsive"
+
+      ></Image>
+
+    </div>
+        <div>
+  <ul>
+<li>
+
+  <h1 className="text-lg">{product.name}</h1>
+</li>
+
+<li> Category:{product.category}</li>
+<li>Brand:{product.brand}</li>
+<li>{product.rating} of {product.numReviews} reviews</li>
+<li>Description: {product.description}</li>
+</ul>
+</div>
+<div>
+  <div className="card p-5">
+    <div className="mb-2 flex justify-between">
+      <div>Price</div>
+      <div>${product.price}</div>
+
+    </div>
+
+  </div>
+</div>
+
+   </div>
 
     </div>
 </Layout>
